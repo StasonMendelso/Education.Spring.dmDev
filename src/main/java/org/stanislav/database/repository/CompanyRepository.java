f65fc84574b37cd16cff46156168ad7f82e8a4d1
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-23 10:51:43 +0200

    annotation-based configuration: implementing custom BeanPostProcessor
    Implement custom BeanPostProcessor for handling @InjectBean annotation.

--- src/main/java/org/stanislav/database/repository/CompanyRepository.java
+++ src/main/java/org/stanislav/database/repository/CompanyRepository.java
@@ -1,18 +1,12 @@
 package org.stanislav.database.repository;
 
+import org.stanislav.bpp.InjectBean;
 import org.stanislav.database.pool.ConnectionPool;
 
 /**
  * @author Stanislav Hlova
  */
 public class CompanyRepository {
-    private final ConnectionPool connectionPool;
-
-    private CompanyRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static CompanyRepository of(ConnectionPool connectionPool) {
-        return new CompanyRepository(connectionPool);
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
