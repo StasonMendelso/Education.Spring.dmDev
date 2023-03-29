e776fb279cc85d0ab3072669c09d76b4c456fc64
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-29 08:47:13 +0300

    spring-boot: notes about @SpringBootApplication annotation
    Make notes about @SpringBootApplication annotation.
    Reduce some configuration code.

--- src/main/java/org/stanislav/spring/ApplicationRunner.java
+++ src/main/java/org/stanislav/spring/ApplicationRunner.java
@@ -2,6 +2,7 @@ package org.stanislav.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Stanislav Hlova
@@ -9,7 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationRunner.class,args);
+         ConfigurableApplicationContext context = SpringApplication.run(ApplicationRunner.class,args);
+        System.out.println(context.getBeanDefinitionNames());
     }
 
 }
