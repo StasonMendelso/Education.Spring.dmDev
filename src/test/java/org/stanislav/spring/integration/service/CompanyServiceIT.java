77b34f299a6e504062b5294e6e8f117d52316f83
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-03-31 10:37:57 +0300

    testing: reduce boilerplate code, enable properties configuration.
    Reduce boilerplate code by adding custom annotation.
    Add posibility of overriding only setted properties.
    Add @AcrttiveProfile annotation for it.
    Make notes about it.

--- src/test/java/org/stanislav/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/org/stanislav/spring/integration/service/CompanyServiceIT.java
@@ -1,5 +1,6 @@
 package org.stanislav.spring.integration.service;
 
+import lombok.RequiredArgsConstructor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
@@ -11,11 +12,15 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.stanislav.spring.ApplicationRunner;
+import org.stanislav.spring.config.DatabaseProperties;
 import org.stanislav.spring.database.entity.Company;
 import org.stanislav.spring.dto.CompanyReadDto;
+import org.stanislav.spring.integration.annotation.IntegrationTest;
 import org.stanislav.spring.listener.entity.EntityEvent;
 import org.stanislav.spring.service.CompanyService;
 
@@ -24,12 +29,14 @@ import java.util.Optional;
 /**
  * @author Stanislav Hlova
  */
-@SpringBootTest
-//@SpringBootTest(classes = ApplicationRunner.class)
+@IntegrationTest
+@RequiredArgsConstructor
 public class CompanyServiceIT {
     public static final Integer COMPANY_ID = 1;
-    @Autowired
-    private CompanyService companyService;
+
+    private final CompanyService companyService;
+
+    private final DatabaseProperties databaseProperties;
 
     @Test
     void findById() {
