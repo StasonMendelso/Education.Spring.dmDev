af60b0de54d7fc3153560a627d3eb59c591bb70a
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-14 09:10:16 +0300

    web-starter: use annotations for getting parameters from request.
    Use @RequestParam, @RequestHeader, @CookieValue, @PathVariable.
    Make notes about http request/response

--- src/main/java/org/stanislav/spring/http/controller/GreetingController.java
+++ src/main/java/org/stanislav/spring/http/controller/GreetingController.java
@@ -1,8 +1,14 @@
 package org.stanislav.spring.http.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -12,8 +18,17 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/greeting")
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request,
+                              @RequestParam("age") Integer age,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable("id") Integer id) {
+        String ageParamValue = request.getParameter("age");
+        String acceptHeader = request.getHeader("accept");
+        Cookie[] cookies = request.getCookies();
+
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
