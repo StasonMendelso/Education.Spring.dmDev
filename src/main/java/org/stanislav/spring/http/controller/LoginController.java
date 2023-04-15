26767642be1ae60efae32137bb9fcf7763388eca
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-15 14:48:56 +0300

    web-starter: notes about Forward-Include-Redirect
    Make notes about Forward-Include-Redirect

--- src/main/java/org/stanislav/spring/http/controller/LoginController.java
+++ src/main/java/org/stanislav/spring/http/controller/LoginController.java
@@ -20,6 +20,6 @@ public class LoginController {
     @PostMapping("/login")
     public String login(Model model,
                         @ModelAttribute("login") LoginDto loginDto) {
-        return "user/login";
+        return "redirect:/login";
     }
 }
