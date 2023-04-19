46f87f8a7e92faf9aceb8767bf81c07ff1c1d579
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-19 11:07:56 +0300

    thymelead-starter: implement create operation for user in view.
    Implement create operation for user in view

--- src/main/java/org/stanislav/spring/http/controller/UserController.java
+++ src/main/java/org/stanislav/spring/http/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.stanislav.spring.database.entity.Role;
 import org.stanislav.spring.dto.UserCreateEditDto;
 import org.stanislav.spring.service.CompanyService;
@@ -46,10 +47,25 @@ public class UserController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("user") UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "user/registration";
+    }
+
     @PostMapping
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(UserCreateEditDto user) {
-        return "redirect:/users/" + userService.create(user);
+    public String create(@ModelAttribute UserCreateEditDto user,
+                         RedirectAttributes redirectAttributes) {
+//        if (true){
+////            redirectAttributes.addAttribute("username", user.getUsername());
+////            redirectAttributes.addAttribute("firstname", user.getFirstname());
+//            redirectAttributes.addFlashAttribute("user", user); // add to session
+//            return "redirect:/users/registration";
+//        }
+        return "redirect:/users/" + userService.create(user).getId();
     }
 
     //    @PutMapping("/{id}")
