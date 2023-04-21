253c15558048f1c2a82a5c54a7680fe5e457e57d
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-04-21 15:40:39 +0300

    rest: implement rest controller with standart endpoints.
    Implement rest controller with standart endpoints

--- src/main/java/org/stanislav/spring/http/handler/RestControllerExceptionHandler.java
+++ src/main/java/org/stanislav/spring/http/handler/RestControllerExceptionHandler.java
@@ -4,18 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
  * @author Stanislav Hlova
  */
 @Slf4j
-@ControllerAdvice(basePackages = "org.stanislav.spring.http.controller")
-public class ControllerExceptionHandler /*extends ResponseEntityExceptionHandler*/ {
-
-    @ExceptionHandler(Exception.class)
-    public String handleException(Exception exception, HttpServletRequest request){
-        log.error("Failed to return response", exception);
-        return "error/error500";
-    }
+@RestControllerAdvice(basePackages = "org.stanislav.spring.http.rest")
+public class RestControllerExceptionHandler extends ResponseEntityExceptionHandler {
 }
---
+++ src/main/java/org/stanislav/spring/http/rest/UserRestController.java
@@ -0,0 +1,77 @@
+package org.stanislav.spring.http.rest;
+
+import jakarta.validation.groups.Default;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.stanislav.spring.database.entity.Role;
+import org.stanislav.spring.dto.PageResponse;
+import org.stanislav.spring.dto.UserCreateEditDto;
+import org.stanislav.spring.dto.UserFilter;
+import org.stanislav.spring.dto.UserReadDto;
+import org.stanislav.spring.service.CompanyService;
+import org.stanislav.spring.service.UserService;
+import org.stanislav.spring.validation.group.CreateAction;
+import org.stanislav.spring.validation.group.UpdateAction;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * @author Stanislav Hlova
+ */
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserRestController {
+    private final UserService userService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public PageResponse<UserReadDto> findAll(Model model, UserFilter userFilter) {
+        List<UserReadDto> list = userService.findAll(userFilter);
+        return PageResponse.of(list);
+    }
+
+    @GetMapping("/{id}")
+    public UserReadDto findById(@PathVariable("id") Long id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserReadDto create(@RequestBody @Validated({Default.class, CreateAction.class}) UserCreateEditDto user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserReadDto update(@PathVariable("id") Long id, @RequestBody @Validated({Default.class, UpdateAction.class}) UserCreateEditDto user) {
+        return userService.update(id, user)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}/delete")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        if (userService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
