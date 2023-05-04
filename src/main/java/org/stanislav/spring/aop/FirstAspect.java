e19c38f97e30f8323c6464b9164ac4e43f9c7b97
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-05-04 12:43:40 +0300

    aop: implement first aspect and Pointcut. Make notes about it.
    Implement first aspect and Poincuts.
    Make notes about Pointcut annotation values.

---
+++ src/main/java/org/stanislav/spring/aop/FirstAspect.java
@@ -0,0 +1,83 @@
+package org.stanislav.spring.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.stanislav.spring.validation.annotation.UserInfo;
+
+
+/**
+ * @author Stanislav Hlova
+ */
+@Aspect
+@Component
+public class FirstAspect {
+
+    /*
+        @within - check annotation on the class level
+     */
+    @Pointcut(value = "@within(org.springframework.stereotype.Controller)")
+    public void isControllerLayer() {
+    }
+
+    /*
+        within - check class type name
+     */
+    @Pointcut(value = "within(org.stanislav.spring.service.*Service)")
+    public void isServiceLayer() {
+    }
+
+    /*
+       this - обращается именно к АОП proxy и его типу, которое будет создано
+       target - обращается к нашему исходному объекту, вокруг которого огорнут прокси и проверяет его тип
+       this - check AOP proxy class type
+       target - check target object class type
+     */
+    @Pointcut(value = "this(org.springframework.stereotype.Repository)")
+    public void isRepositoryLayer() {
+    }
+
+    /*
+        @annotation - check annotation on the method level
+     */
+    @Pointcut(value = "isControllerLayer() && @annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void hasGetMapping() {
+    }
+
+    /*
+        args - check method param type
+        * - any param type
+        .. - any param type from 0..*
+     */
+
+    @Pointcut(value = "isControllerLayer() && args(org.springframework.ui.Model,..)")
+//    @Pointcut(value = "args(org.springframework.ui.Model,*)") ровно два аргумента
+    public void hasFirstModelParam() {
+    }
+
+    /*
+      @args - check annotation on the param type, not annotations near param.
+      * - any param type
+      .. - any param type from 0..*
+   */
+    @Pointcut(value = "isControllerLayer() && @args(org.stanislav.spring.validation.annotation.UserInfo)")
+    public void hasUserInfoParamAnnotation() {
+    }
+
+    /*
+        bean - check bean name
+     */
+    @Pointcut(value = "bean(*Service)")
+    public void isServiceLayerBean() {
+    }
+
+    /*
+    execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern) throws-pattern?)
+     */
+    @Pointcut(value = "execution(public * org.stanislav.spring.service.*Service.findById(*))")
+//    @Pointcut(value = "execution(public * findById(*))")
+    public void anyFindByIdServiceMethod() {
+    }
+}
