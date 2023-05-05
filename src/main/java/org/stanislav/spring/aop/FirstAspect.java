a4956932d1e081a1f42800764e392b20bd263c97
Stanislav Hlova <stanislav.hlova@gmail.com>
2023-05-05 10:58:07 +0300

    aop: implement @Around advice
    Implement @Around advice

--- src/main/java/org/stanislav/spring/aop/FirstAspect.java
+++ src/main/java/org/stanislav/spring/aop/FirstAspect.java
@@ -2,9 +2,11 @@ package org.stanislav.spring.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -125,6 +127,27 @@ public class FirstAspect {
             argNames = "service")
     public void addLoggingAfterFinally(Object service) {
         log.info("after (finally) - invoked findById method in class {}", service);
+    }
+    @Around(value = "anyFindByIdServiceMethod()" +
+                    "&& args(id) " +
+                    "&& target(service)",
+            argNames = "proceedingJoinPoint,service,id")
+    public Object addLoggingAround(ProceedingJoinPoint proceedingJoinPoint,
+                                   Object service,
+                                   Object id) throws Throwable {
+        log.info("AROUND before invoked findById method in class {}, with id {}", service, id);
+        try{
+            Object result = proceedingJoinPoint.proceed();
+            // WE MUST ALWAYS THROW EXCEPTION. ANOTHER INTERCEPTORS MUST SEE EXCEPTION
+            log.info("AROUND after returning - invoked findById method in class {}, with result {}", service, result);
+            return result;
+
+        }catch (Throwable exception){
+            log.info("after throwing - invoked findById method in class {}, with exception {}: {}", service, exception.getClass(), exception.getMessage());
+            throw exception;
+        }finally {
+            log.info("AROUND after (finally) - invoked findById method in class {}", service);
+        }
     }
 
 }
